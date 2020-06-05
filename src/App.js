d53bfa73c0fb83449dbb329fb6678674959fb41d
Rameez Qazi <38956101+rameezqazi23@users.noreply.github.com>
2020-06-06 00:37:52 +0500

    update file app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br/>
+          Created by Rameez Qazi
         </p>
         <a
           className="App-link"
